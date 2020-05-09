eb5d3320117b84d91d8ea41466a676476b90ea56
xq114 <1140735506@qq.com>
2020-05-09 15:57:51 +0800

    modified makefile & add libtorch explaination

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # MajhongBot
 第12届北京大学游戏对抗赛麻将AI
 
+# 第三方库说明
+用到麻将的算番库，源码依赖，放在utils目录下；
+用到jsoncpp库，可以使用包管理器安装；
+用到libtorch库，安装方法：
+
+```shell
+# linux
+cd /usr/local/src
+wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.5.0%2Bcpu.zip
+unzip libtorch-cxx11-abi-shared-with-deps-1.5.0%2Bcpu.zip
+```
+
+![Mac OS 预编译包](https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.5.0.zip)，解压到/usr/local/src即可（未测试）
+
 # 编译说明
 提交要求输出单文件，输出到build/fbot.cpp（目前单文件不需要，直接提交bot.cpp即可）
 
--- src/botnn.cpp
+++ src/botnn.cpp
@@ -2,7 +2,35 @@
 #include <torch/torch.h>
 using namespace std;
 
+constexpr size_t CHANNEL_SIZE = 9 + 9 + 9 + 4 + 3; // 34
+using channel = bool[CHANNEL_SIZE];
+
+struct NNInput {
+    channel hand[4];
+    channel pack[4][4];
+    channel menfeng[4];
+    channel quanfeng[4];
+    channel discarded[4][30][4];
+    channel ifeng[4];
+    channel idiscard[4];
+    channel imo[4];
+
+    channel lshanten[5];
+    channel dshanten[5];
+    channel probten[10];
+    channel averagefan[10];
+    channel shanten[5][10];
+};
+
+class Agent {
+    NNInput input;
+
+  public:
+    Agent() {}
+};
+
 int main() {
     torch::Tensor tensor = torch::rand({2, 3});
     cout << tensor << endl;
+    cout << sizeof(channel) << " " << sizeof(NNInput) << endl;
 }
