4655d2ac479abc973d4b7f2ac042879903740e92
xq114 <1140735506@qq.com>
2020-05-23 23:08:53 +0800

    add 2 more models & update model

--- src/nn/mjdata.py
+++ src/nn/mjdata.py
@@ -25,7 +25,9 @@ if __name__ == '__main__':
     if len(sys.argv) == 3 and sys.argv[1] == 'sample':
         print('sampling {} data files...'.format(sys.argv[2]))
         nsamples = int(sys.argv[2])
-        get_filenames_split(nsamples, 0.2 if nsamples < 100 else 0.1)
+        ratio = 0.2 if nsamples < 100 else 0.1
+        print("val_ratio =", ratio)
+        get_filenames_split(nsamples, val_ratio=ratio)
     elif len(sys.argv) == 2 and sys.argv[1] == 'sample':
         print('sampling 10 data files...')
         get_filenames_split(10)
